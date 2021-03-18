cc7449efeb7cc1296dff8788b8b7c931674f04bf
Alienmaster <mitachundkrach@gmail.com>
2021-03-18 22:14:30 +0000

    changes for websocket

--- kaldi_starter.py
+++ kaldi_starter.py
@@ -7,14 +7,13 @@ import argparse
 
 
 def start_kaldi(server, input, output, controlChannel, speaker):
-    chDir = "/home/bbb/ba/kaldi_modelserver_bbb"
-    kaldiDir = "pykaldi_bbb_env/bin/python3.7 nnet3_model.py -m 0 -e -t -y models/kaldi_tuda_de_nnet3_chain2.yaml --redis-server=%s --redis-audio=%s --redis-channel=%s --redis-control=%s -s='%s' -fpc 190" % (server, input, output, controlChannel, speaker)
+    chDir = "/home/6geislin/kaldi-model-server"
+    kaldiDir = "kms_env/bin/python3.7 nnet3_model.py -m 0 -e -t -y models/kaldi_tuda_de_nnet3_chain2.yaml --redis-server=%s --redis-audio=%s --redis-channel=%s --redis-control=%s -s='%s' -fpc 190" % (server, input, output, controlChannel, speaker)
     os.chdir(chDir)
     os.system(kaldiDir)
 
 
 def wait_for_channel(server, port, channel):
-    kaldiInstances = {}
     red = redis.Redis(host=server, port=port, password="")
     pubsub = red.pubsub()
     pubsub.subscribe(channel)
@@ -23,67 +22,42 @@ def wait_for_channel(server, port, channel):
         time.sleep(0.2)
         message = pubsub.get_message()
         if message and message["data"] != 1:
-            message = json.loads(message["data"].decode("UTF-8"))
             try:
-                meetingId = message["meetingId"]
+                message = json.loads(message["data"].decode("UTF-8"))
+                print(message)
                 callerUsername = message["Caller-Username"]
-                channelName = meetingId + "%" + callerUsername.replace(" ", ".")
-                inputChannel = channelName + "%asr"
-                outputChannel = channelName + "%data"
-                controlChannel = channelName + "%control"
+                audioChannel = message["Audio-Channel"]
+                textChannel = message["Text-Channel"]
+                controlChannel = message["Control-Channel"]
                 callerDestinationNumber = message["Caller-Destination-Number"]
                 origCallerIDName = message["Caller-Orig-Caller-ID-Name"]
                 if message["Event"] == "LOADER_START":
                     print("Start Kaldi")
-                    p = mp.Process(target=start_kaldi, args=(server, inputChannel, outputChannel, controlChannel, callerUsername))
+                    p = mp.Process(target=start_kaldi, args=(server, audioChannel, textChannel, controlChannel, callerUsername))
                     p.start()
-                    kaldiInstances[inputChannel] = p
-
-                    Kaldi_Start_msg = {
-                                        "Event": "KALDI_START",
-                                        "Caller-Destination-Number": callerDestinationNumber,
-                                        "meetingId": meetingId,
-                                        "Caller-Orig-Caller-ID-Name": origCallerIDName,
-                                        "Caller-Username": callerUsername,
-                                        "Input-Channel": inputChannel,
-                                        "ASR-Channel": outputChannel,
-                                        "Control-Channel": controlChannel
-                                        }
-                    red.publish(channel, json.dumps(Kaldi_Start_msg))
-
+                    # kaldiInstances[audioChannel] = p
+                    redis_channel_message(red, channel, "KALDI_START", callerDestinationNumber, origCallerIDName, callerUsername, audioChannel, textChannel, controlChannel)
+                    
                 if message["Event"] == "LOADER_STOP":
-                    inputChannel = message["ASR-Channel"]
+                    audioChannel = message["Audio-Channel"]
                     print("Stop Kaldi")
                     red.publish(controlChannel, "shutdown")
-                    Kaldi_Stop_msg = {
-                                        "Event": "KALDI_STOP",
-                                        "Caller-Destination-Number": callerDestinationNumber,
-                                        "meetingId": meetingId,
-                                        "Caller-Orig-Caller-ID-Name": origCallerIDName,
-                                        "Caller-Username": callerUsername,
-                                        "Input-Channel": inputChannel,
-                                        "ASR-Channel": outputChannel,
-                                        "Control-Channel": controlChannel
-                                        }
-                    red.publish(channel, json.dumps(Kaldi_Stop_msg))
-
-                    # p = kaldiInstances.pop(inputChannel, None)
-                    # if p:
-                    #     p.terminate()  # TODO: Problems with orphaned processes. Eventually call Kaldi as a module and not with the system
-                    #     p.join()
-                    #     Kaldi_Stop_msg = {
-                    #                        "Event": "KALDI_STOP",
-                    #                        "Caller-Destination-Number": callerDestinationNumber,
-                    #                        "meetingId": meetingId,
-                    #                        "Caller-Orig-Caller-ID-Name": origCallerIDName,
-                    #                        'Caller-Username': callerUsername,
-                    #                        "Input-Channel": inputChannel,
-                    #                        "ASR-Channel": outputChannel,
-                    #                        "Control-Channel": controlChannel
-                    #                        }
-                    #     red.publish(channel, json.dumps(Kaldi_Stop_msg))
+                    redis_channel_message(red, channel, "KALDI_STOP", callerDestinationNumber, origCallerIDName, callerUsername, audioChannel, textChannel, controlChannel)
             except:
                 pass
+
+
+def redis_channel_message(red, channel, Event, callerDestinationNumber, origCallerIDName, callerUsername, inputChannel, outputChannel, controlChannel):
+    message = {
+                "Event": Event,
+                "Caller-Destination-Number": callerDestinationNumber,
+                "Caller-Orig-Caller-ID-Name": origCallerIDName,
+                "Caller-Username": callerUsername,
+                "Audio-Channel": inputChannel,
+                "Text-Channel": outputChannel,
+                "Control-Channel": controlChannel
+    }
+    red.publish(channel, json.dumps(message))
 
 
 if __name__ == "__main__":
