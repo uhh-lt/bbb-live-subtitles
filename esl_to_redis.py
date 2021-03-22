2f2bf0acc931ba185a3e4da925c60057fa5276b6
Alienmaster <mitachundkrach@gmail.com>
2021-03-22 13:56:42 +0100

    removed legacy code

--- esl_to_redis.py
+++ esl_to_redis.py
@@ -79,41 +79,6 @@ def del_member(event):
 #     print(event)
 #     app.command(command="uuid_audio_fork " + uuid + " start ws://localhost:3001 mono 16k teeeeest", background=False)
 
-# @app.handle('MEDIA_BUG_START')
-# def media_bug_start(event):
-#     logger.info("MEDIA_BUG_START")
-#     logger.debug(event)
-#     callerDestinationNumber = event["Caller-Destination-Number"].replace("echo", "")
-#     Event = event["Event-Name"]
-#     origCallerIDName = event["Caller-Orig-Caller-ID-Name"]
-#     mediaBugTarget = event["Media-Bug-Target"]
-#     mediaBugFunction = event["Media-Bug-Function"]
-#     callerUsername = origCallerIDName.partition("-bbbID-")[2]
-#     if mediaBugFunction == "session_record":
-#         start_MB = {"Event": Event,
-#                     "Caller-Destination-Number": callerDestinationNumber,
-#                     "Caller-Orig-Caller-ID-Name": origCallerIDName,
-#                     "Caller-Username": callerUsername,
-#                     "Media-Bug-Target": mediaBugTarget}
-#         send_to_pubsub(start_MB)
-
-
-# @app.handle('MEDIA_BUG_STOP')
-# def media_bug_stop(event):
-    # logger.info("MEDIA_BUG_STOP")
-    # # logger.debug(event)
-    # CallerDestinationNumber = event["Caller-Destination-Number"].replace("echo", "")
-    # Event = event["Event-Name"]
-    # OrigCallerIDName = event["Caller-Orig-Caller-ID-Name"]
-    # Media_Bug_Target = event["Media-Bug-Target"]
-    # Media_Bug_Function = event["Media-Bug-Function"]
-    # CallerUsername = OrigCallerIDName.partition("-bbbID-")[2]
-
-    # if Media_Bug_Function == "session_record":
-    #     stop_MB = {"Event": Event, "Caller-Destination-Number": CallerDestinationNumber, "Caller-Orig-Caller-ID-Name": OrigCallerIDName, "Caller-Username": CallerUsername, "Media-Bug-Target": Media_Bug_Target}
-    #     send_to_pubsub(stop_MB)
-
-
 def send_to_pubsub(data):
     logger.debug("Redis Message to " + asr_channel + " :")
     logger.debug(data)
