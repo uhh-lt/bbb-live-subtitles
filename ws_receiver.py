b98620486a20d72c7ffbd6334a704c125dc0df31
Alienmaster <mitachundkrach@gmail.com>
2021-08-01 21:22:27 +0200

    minor changes

--- ws_receiver.py
+++ ws_receiver.py
@@ -5,8 +5,8 @@ import json
 import logging
 from urllib import parse
 
-port = "3001"
-asr_channel = "asr_channel"
+port = '3001'
+asr_channel = 'asr_channel'
 
 logging.basicConfig(
     level=logging.INFO,
@@ -15,52 +15,52 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-red = redis.Redis(host="localhost", port=6379, password="")
+red = redis.Redis(host='localhost', port=6379, password='')
 
 
 async def socket_to_redis(websocket, path):
     path = parse.unquote(path[1:])
-    logger.info("Websocket Connection established")
-    logger.info("Connected to: " + path)
-    callerDestinationNumber = path.split("/")[0]
-    origCallerIDName = path.split("/")[1]
-    callerUsername = origCallerIDName.split("-bbbID-")[1]
-    if len(origCallerIDName.split("-bbbID-")[1].rsplit("_", 1)) > 1:
-        language = origCallerIDName.split("-bbbID-")[1].rsplit("_", 1)[1]
-        if  language.startswith("E"):
-            language = "English"
+    logger.info('Websocket Connection established')
+    logger.info('Connected to: ' + path)
+    callerDestinationNumber = path.split('/')[0]
+    origCallerIDName = path.split('/')[1]
+    callerUsername = origCallerIDName.split('-bbbID-')[1]
+    if len(origCallerIDName.split('-bbbID-')[1].rsplit('_', 1)) > 1:
+        language = origCallerIDName.split('-bbbID-')[1].rsplit('_', 1)[1]
+        if  language.startswith('E'):
+            language = 'English'
         else:
-            language = "German"
+            language = 'German'
     else:
-        language = "German"
-    audioChannel = parse.quote(callerDestinationNumber + "~" + origCallerIDName) + "~audio"
-    controlChannel = parse.quote(callerDestinationNumber + "~" + origCallerIDName) + "~control"
-    textChannel = parse.quote(callerDestinationNumber + "~" + origCallerIDName) + "~text"
-    redis_message("LOADER_START", callerDestinationNumber, origCallerIDName, callerUsername, language, audioChannel, controlChannel, textChannel)
+        language = 'German'
+    audioChannel = parse.quote(callerDestinationNumber + '~' + origCallerIDName) + '~audio'
+    controlChannel = parse.quote(callerDestinationNumber + '~' + origCallerIDName) + '~control'
+    textChannel = parse.quote(callerDestinationNumber + '~' + origCallerIDName) + '~text'
+    redis_message('LOADER_START', callerDestinationNumber, origCallerIDName, callerUsername, language, audioChannel, controlChannel, textChannel)
 
     async for message in websocket:
         red.publish(audioChannel, message)
-    logger.info("Connection %s closed" % path)
-    redis_message("LOADER_STOP", callerDestinationNumber, origCallerIDName, callerUsername, language, audioChannel, controlChannel, textChannel)
+    logger.info('Connection %s closed' % path)
+    redis_message('LOADER_STOP', callerDestinationNumber, origCallerIDName, callerUsername, language, audioChannel, controlChannel, textChannel)
 
 
 def redis_message(event, callerDestinationNumber, origCallerIDName, callerUsername, language, audioChannel, controlChannel, textChannel):
     message = {
-                "Event": event,
-                "Caller-Destination-Number": callerDestinationNumber,
-                "Caller-Orig-Caller-ID-Name": origCallerIDName,
-                "Caller-Username": callerUsername,
-                "Language" : language,
-                "Audio-Channel": audioChannel,
-                "Control-Channel": controlChannel,
-                "Text-Channel": textChannel,
+                'Event': event,
+                'Caller-Destination-Number': callerDestinationNumber,
+                'Caller-Orig-Caller-ID-Name': origCallerIDName,
+                'Caller-Username': callerUsername,
+                'Language' : language,
+                'Audio-Channel': audioChannel,
+                'Control-Channel': controlChannel,
+                'Text-Channel': textChannel,
               }
     logger.info(message)
     red.publish(asr_channel, json.dumps(message))
 
 
-start_server = websockets.serve(socket_to_redis, "ltbbb2", port)
+start_server = websockets.serve(socket_to_redis, 'YOUR BBB SERVER', port)
 
 asyncio.get_event_loop().run_until_complete(start_server)
-logger.info("Websocket Server started on Port " + port)
+logger.info('Websocket Server started on Port ' + port)
 asyncio.get_event_loop().run_forever()
